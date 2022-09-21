7d3e905700e45e5c2aa92f104a34a5fbfab72ad9
gsygiardini <113537151+gsygiardini@users.noreply.github.com>
2022-09-21 06:21:46 -0300

    Update simulation.py

--- cell_mig/simulation.py
+++ cell_mig/simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from time import sleep
 from scipy.signal import convolve2d
 
 class pdf(object):
@@ -7,24 +8,24 @@ class pdf(object):
     self.y = y
     self.theta = theta
     self.kappa = kappa
-    self.prob = prob
     self.lattice = np.zeros([self.x+2, self.y+2], dtype='float')
-  
-  def diffusion_para_dir(self): 
+    self.lattice = initial_grid
+
+  def diffusion_para_dir(self):
     kernel=np.array([[0.5  , 0,   0.5]])
     self.lattice = convolve2d(self.lattice,kernel,boundary='wrap')
-    
+
   def diffusion_perp_dir(self):
     kernel=np.array([[0.5],
                     [0],
                     [0.5]])
     self.lattice = convolve2d(self.lattice,kernel,boundary='wrap')
-    
+
   def drift_para_dir(self):
-    self.lattice = np.roll(self.lattice, 1, axis=0)
-    
+    self.lattice = np.roll(self.lattice, -1, axis=0)
+
   def drift_perp_dir(self):
-    self.lattice = np.roll(self.lattice, 1, axis=0)
+    self.lattice = np.roll(self.lattice, 1, axis=1)
     
 #   def theta_diffusion(self,):
     
