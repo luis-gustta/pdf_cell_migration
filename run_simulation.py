483877d5fa7706e4432e44d9003a74849f190045
gsygiardini <113537151+gsygiardini@users.noreply.github.com>
2022-11-05 03:05:42 -0300

    Update run_simulation.py

--- run_simulation.py
+++ run_simulation.py
@@ -2,12 +2,21 @@ import numpy as np
 from time import sleep
 from cell_mig import pdf
 
-ini_grid = create_grid(50,50,8,8)
-ini_grid[25,25,:,:] = 1.
+total_time = 1000
+N_x = 50
+N_y = 50
+N_theta = 8
+N_vel = 8
+
+ini_grid = create_grid(N_x, N_y, N_theta, N_vel)
+ini_grid[N_x/2., N_y/2.,:,:] = 1.
 
 cell1 = cell(ini_grid)
+cell1.D_theta = 0.05
+cell1.D_para_dir = 0.05
+cell1.D_perp_dir = 0.05
 
-for time in range(0,1000):
+for time in range(0,total_time):
     cell1.drift_para_dir()
     cell1.drift_perp_dir()
     cell1.diffusion_para_dir()
