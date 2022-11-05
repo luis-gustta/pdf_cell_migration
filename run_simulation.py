04977c2d1241a720129ffa61d33bc18bf8bd7613
gsygiardini <113537151+gsygiardini@users.noreply.github.com>
2022-11-05 02:59:42 -0300

    Update run_simulation.py

--- run_simulation.py
+++ run_simulation.py
@@ -2,26 +2,28 @@ import numpy as np
 from time import sleep
 from cell_mig import pdf
 
+ini_grid = create_grid(50,50,8,8)
+ini_grid[25,25,:,:] = 1.
 
-ini_grid = np.array([
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,1.,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0]])
+cell1 = cell(ini_grid)
 
-cell1 = pdf.cell(10,10,0,0,ini_grid)
-
-for i in range(0,10):
-    #cell1.drift_para_dir()
-    #cell1.drift_perp_dir()
+for time in range(0,1000):
+    cell1.drift_para_dir()
+    cell1.drift_perp_dir()
     cell1.diffusion_para_dir()
     cell1.diffusion_perp_dir()
-    print(cell1.lattice)
-    print("\n")
-    sleep(1)
+
+    cell1.to_real_lattice()
+    cell1.diffusion_theta()
+    cell1.from_real_lattice()
+    cell1.print_state()
+
+    if time%15==0 :
+        plt.clf()
+        plt.cla()
+        plt.close('all')
+        print(cell1.total())
+
+plt.clf()
+plt.cla()
+plt.close('all')
