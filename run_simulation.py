b100759622cc679219a1a6515093469fe86bc2b5
gsygiardini <113537151+gsygiardini@users.noreply.github.com>
2023-01-24 11:06:34 -0300

    Update run_simulation.py

--- run_simulation.py
+++ run_simulation.py
@@ -2,37 +2,46 @@ import numpy as np
 from time import sleep
 from cell_mig import pdf
 
-total_time = 1000
-N_x = 50
-N_y = 50
-N_theta = 8
-N_vel = 8
+file = open('prob_sum.dat', 'w')
 
-ini_grid = create_grid(N_x, N_y, N_theta, N_vel)
-ini_grid[N_x/2., N_y/2.,:,:] = 1.
+total_time = 1000
+
+N = 50
+ini_grid = create_grid(N,N,12,12)
+ini_grid[int(N/2.),int(N/2.),0,0] = 1.
 
 cell1 = cell(ini_grid)
-cell1.D_theta = 0.05
-cell1.D_para_dir = 0.05
-cell1.D_perp_dir = 0.05
+cell1.D_theta = 0.9
+cell1.D_para_dir = 0.9
+cell1.D_perp_dir = 0.9
+cell1.kappa = 1.
+cell1.gamma = 1.
 
 for time in range(0,total_time):
-    cell1.drift_para_dir()
-    cell1.drift_perp_dir()
-    cell1.diffusion_para_dir()
-    cell1.diffusion_perp_dir()
 
-    cell1.to_real_lattice()
+    cell1.drift_para_dir()
+    # cell1.real_periodic_boundary()
+    cell1.diffusion_para_dir()
+    # cell1.real_periodic_boundary()
+    cell1.diffusion_perp_dir()
+    # cell1.polarization_dynamics_dissipation()
+    cell1.real_periodic_boundary()
+
+    cell1.to_real_lattice() #-----> look https://gautamnagrawal.medium.com/rotating-image-by-any-angle-shear-transformation-using-only-numpy-d28d16eb5076
     cell1.diffusion_theta()
     cell1.from_real_lattice()
     cell1.print_state()
 
-    if time%15==0 :
-        plt.clf()
-        plt.cla()
+    temp = str(time)+" "+str(cell1.total())
+    print(temp,file=file)
+    print(temp)
+
+    if time%10==0 :
+        # plt.clf()
+        # plt.cla()
         plt.close('all')
-        print(cell1.total())
 
 plt.clf()
 plt.cla()
 plt.close('all')
+file.close()
